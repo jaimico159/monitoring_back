2d0aa2dd85dbe54f6c06494f6f6a73adf7da00aa
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-08 11:12:03 -0500

    creates engineer model

---
+++ db/migrate/20220108161141_create_engineers.rb
@@ -0,0 +1,11 @@
+class CreateEngineers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :engineers do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :display_name
+
+      t.timestamps
+    end
+  end
+end
