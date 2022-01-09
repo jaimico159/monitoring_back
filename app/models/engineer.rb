c5623c2849e50b36c2eeb0e90eb2ead4f8a5e84d
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-09 10:50:37 -0500

    adds validations to engineers

--- app/models/engineer.rb
+++ app/models/engineer.rb
@@ -1,4 +1,8 @@
 class Engineer < ApplicationRecord
   has_many :contracts, through: :contract_engineers
   has_many :reservations
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :display_name
 end
