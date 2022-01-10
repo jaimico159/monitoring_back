ed94a1cecc8a46e2a36215b0a8925ac266f83be7
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-09 22:21:44 -0500

    adds engineers to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Engineer.create!([
+                   {
+                     first_name: 'Barbara',
+                     last_name: 'Aliaga',
+                     display_name: 'Barbara A.'
+                   },
+                   {
+                     first_name: 'Lennart',
+                     last_name: 'Sönke',
+                     display_name: 'Lennart S.'
+                   },
+                   {
+                     first_name: 'Benjamin',
+                     last_name: 'Cuadros',
+                     display_name: 'Benjamin C.'
+                   }
+                 ])
