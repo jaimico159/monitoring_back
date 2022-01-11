4a66b56ff55d926540bb063b53a902c38e0d9db1
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-11 09:12:26 -0500

    creates contract plan builder

---
+++ app/utils/builders/contract/contract_plan_builder.rb
@@ -0,0 +1,51 @@
+module Builders
+  module Contract
+    class ContractPlanBuilder
+      def initialize(contract:, from:, to:, options:)
+        @contract = contract
+        @from = from
+        @to = to
+        @options = options
+      end
+
+      def build
+        week = @from
+        while week <= @to
+          build_contract_plan(contract: @contract, start_date: week.beginning_of_week, end_date: week.end_of_week)
+
+          week += 1.week
+        end
+      end
+
+      private
+
+      def build_contract_plan(contract:, start_date:, end_date:)
+        contract_plan = ContractPlan.create(contract_id: contract.id, slot_duration: contract.slot_duration, start_date: start_date,
+                                            end_date: end_date)
+
+        while start_date <= end_date
+          day_option = @options[start_date.wday]
+          if day_option.blank?
+            start_date += 1.day
+            next
+          end
+
+          build_slots(contract_plan: contract_plan, date: start_date, duration: contract_plan.slot_duration, start_at: day_option[:start_at],
+                      end_at: day_option[:end_at])
+
+          start_date += 1.day
+        end
+      end
+
+      def build_slots(contract_plan:, date:, duration:, start_at:, end_at:)
+        position_start = date.to_time.change({ hour: start_at })
+        position_end = date.to_time.change({ hour: end_at })
+        while position_start < position_end
+          TimeSlot.create!(start_at: position_start, duration: duration, contract_id: contract_plan.contract_id,
+                           contract_plan_id: contract_plan.id)
+          position_start += duration
+        end
+      end
+    end
+  end
+end
