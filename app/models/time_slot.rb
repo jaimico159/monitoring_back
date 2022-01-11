3d8e4075a5bca84bb9e351c41ff1dd2d805e93d6
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-11 09:37:31 -0500

    updates spec

--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,4 +2,7 @@ class TimeSlot < ApplicationRecord
   belongs_to :contract
   belongs_to :contract_plan
   belongs_to :engineer, optional: true
+
+  validates_presence_of :duration
+  validates_presence_of :start_at
 end
--- spec/models/time_slot_spec.rb
+++ spec/models/time_slot_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe TimeSlot, type: :model do
   it { should belong_to(:contract_plan) }
   it { should belong_to(:contract) }
-  it { should belong_to(:engineer) }
+  it { should belong_to(:engineer).optional }
+  it { should validate_presence_of(:duration) }
+  it { should validate_presence_of(:start_at) }
 end
