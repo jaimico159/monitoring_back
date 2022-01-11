571c42d6cedd132084e7f1606300e111fb79e765
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-11 08:16:04 -0500

    adds specs to time slots

--- spec/models/time_slot_spec.rb
+++ spec/models/time_slot_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TimeSlot, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:contract_plan) }
+  it { should belong_to(:contract) }
+  it { should belong_to(:engineer) }
 end
