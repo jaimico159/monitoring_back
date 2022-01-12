e4bdb36bccde8c6891b3f9c5f3ae8c104bc0413c
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 11:39:10 -0500

    adds contract plan day to builder

--- app/utils/builders/contract/contract_plan_builder.rb
+++ app/utils/builders/contract/contract_plan_builder.rb
@@ -30,19 +30,24 @@ module Builders
             next
           end
 
-          build_slots(contract_plan: contract_plan, date: start_date, duration: contract_plan.slot_duration, start_at: day_option[:start_at],
-                      end_at: day_option[:end_at])
-
+          build_contract_plan_day(contract_plan: contract_plan, date: start_date, day_option: day_option)
           start_date += 1.day
         end
       end
 
-      def build_slots(contract_plan:, date:, duration:, start_at:, end_at:)
+      def build_contract_plan_day(contract_plan:, date:, day_option:)
+        contract_plan_day = ContractPlanDay.create(current_date: date, contract_plan_id: contract_plan.id)
+        build_slots(contract_plan_day: contract_plan_day, date: date, duration: contract_plan.slot_duration,
+                    start_at: day_option[:start_at],
+                    end_at: day_option[:end_at])
+      end
+
+      def build_slots(contract_plan_day:, date:, duration:, start_at:, end_at:)
         position_start = date.to_time.change({ hour: start_at })
         position_end = date.to_time.change({ hour: end_at })
         while position_start < position_end
-          TimeSlot.create!(start_at: position_start, duration: duration, contract_id: contract_plan.contract_id,
-                           contract_plan_id: contract_plan.id)
+          TimeSlot.create!(start_at: position_start, duration: duration, contract_id: @contract.id,
+                           contract_plan_id: contract_plan_day.contract_plan_id, contract_plan_day_id: contract_plan_day.id)
           position_start += duration
         end
       end
