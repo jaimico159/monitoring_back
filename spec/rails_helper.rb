25081591f3bda9ef38b302608408f1cec595139f
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 18:38:50 -0500

    adds config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -86,6 +86,8 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include ApiHelpers, type: :request
+
+  config.include RSpec::Rails::RequestExampleGroup, type: :request, file_path: %r{spec/api}
 end
 
 Shoulda::Matchers.configure do |config|
