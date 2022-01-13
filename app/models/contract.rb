97a5d497c860a849730a14e3fc78592ef6171f11
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 14:58:33 -0500

    adds validation

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -5,4 +5,8 @@ class Contract < ApplicationRecord
   has_many :time_slots
   has_many :reservations, through: :time_slots
   has_many :contract_plans
+
+  validates_presence_of :slot_duration
+  validates_presence_of :start_at
+  validates_presence_of :end_at
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Contract, type: :model do
   it { should have_many(:contract_plans) }
   it { should belong_to(:company) }
+  it { should validate_presence_of(:slot_duration) }
+  it { should validate_presence_of(:start_at) }
+  it { should validate_presence_of(:end_at) }
 end
