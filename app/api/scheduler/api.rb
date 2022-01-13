26b7352291d0370e8ac24858119f3db1b710957c
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 03:49:14 -0500

    mounts contracts

--- app/api/scheduler/api.rb
+++ app/api/scheduler/api.rb
@@ -2,6 +2,7 @@ module Scheduler
   class API < Grape::API
     mount Scheduler::V1::Engineers
     mount Scheduler::V1::Companies
+    mount Scheduler::V1::Contracts
     mount Scheduler::V1::ContractPlans
   end
 end
