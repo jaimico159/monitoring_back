6c8a596cf82507cd642d3998f4e56b08cffee2ed
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 16:24:41 -0500

    adds specs

--- app/models/contract_plan_day.rb
+++ app/models/contract_plan_day.rb
@@ -1,4 +1,5 @@
 class ContractPlanDay < ApplicationRecord
   belongs_to :contract_plan
   has_many :time_slots
+  validates_presence_of :current_date
 end
--- spec/models/contract_plan_day_spec.rb
+++ spec/models/contract_plan_day_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe ContractPlanDay, type: :model do
+  it { should belong_to(:contract_plan) }
+  it { should have_many(:time_slots) }
+  it { should validate_presence_of(:current_date) }
 end
